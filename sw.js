16566916cc5f1e73a928f0eaadf31989b3d5fca7
ian1021228 <ianw.solar@gmail.com>
2026-05-23 20:50:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,13 +8,27 @@ const urlsToCache = [
     `${BASE_URL}/icon.png`
 ];
 
-// 安裝 Service Worker
+// 安裝 Service Worker，並在安裝後立即刪除舊快取
 self.addEventListener('install', event => {
+    self.skipWaiting(); // 強制等待中的 SW 進入啟動狀態
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
 
+// 啟動 Service Worker，並立即取得頁面控制權
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+// 攔截請求
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
 // 攔截請求，確保這能啟動安裝程序
 self.addEventListener('fetch', event => {
     event.respondWith(
